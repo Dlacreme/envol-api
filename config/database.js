d28892802f9906656db6ae956315fa32c2258143
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2020-06-05 11:47:36 +0200

    setup heroku deployment

--- config/database.js
+++ config/database.js
@@ -2,14 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: `${process.env.DATABASE_HOST}`,
+          port: `${process.env.DATABASE_PORT}`,
+          database: `${process.env.DATABASE_NAME}`,
+          username: `${process.env.DATABASE_USERNAME}`,
+          password: `${process.env.DATABASE_PASSWORD}`,
+          ssl: { "rejectUnauthorized": false }
+        },
+        options: {}
+      }
   },
 });
